883e98e32a50236351455e95e692d6cacddfd893
arjunpratap259-ship-it <arjunpratap259@gmail.com>
2025-11-22 11:41:10 +0530

    Add files via upload

---
+++ least marks of students using function switch.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+struct student{
+	char Name[30];
+	int Rollno;
+	float Marks;
+};
+ struct student Arjun;
+    printf("Enter Name:");
+    scanf("%24s", Arjun.Name);   
+    printf("Enter Rollno: ");
+    scanf("%d", &Arjun.Rollno);     
+    printf("Enter Marks: ");
+    scanf("%f", &Arjun.Marks;  
+    printf("\n--- Student Details ---\n");
+    printf("Name: %s\n", 
+	Arjun.Name);
+    printf("Age: %d\n", Arjun.Rollno);
+    printf("Cutoff: %.2f\n"- Arjun.Marks);
+	int leaststudentmarks(int a,int b,int c){
+return 30;		
+}int main{
+	int s1,s2,s3;
+	s1=Arjun;
+	a=Roll no,marks;
+	s2=diwith;
+	b=Roll no,marks;
+	s3=sai;
+	c=Roll no,marks;
+}int details=3;
+switch(details)
+{
+	case 1:printf("Name Arjun,Roll no 1,Marks 20");
+	break;
+	case 2:printf("Name diwith,Roll no 2,Marks 30");
+	break;
+	case 3:printf("Name sai,Roll no 3,marks 40");
+	break;
+	default:
+		printf("least marks among them");
+}	
+if(marks<40){
+	printf("the deatils of that student");
+	
+}else if(marks<30){
+	printf("the deatils of that student ");
+}else{
+	printf("who is least");
+}
+	
+
